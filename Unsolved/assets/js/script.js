f77dcc333ffc42b378f318de17ff7ca1e939ba4f
Singharaj Usai <san1999@att.net>
2022-06-24 04:00:09 -0400

    add

---
+++ Unsolved/assets/js/script.js
@@ -0,0 +1,28 @@
+// Use Moment.js to format the date and assign to the declared variable.
+// TODO: 1. What is your graduation date in the following format: Jan 1st, 1999?
+var gradDate;
+$("#1a").text(gradDate);
+
+// TODO: 2. What day of the week will 1/1/2022 be?
+var weekDay;
+$("#2a").text(weekDay);
+
+// TODO: 3. Out of 365, what day of the year is today?
+var now;
+//var now = moment().format("DDD");
+$("#3a").text(now);
+
+// TODO: 4. What is the current time in the format: hours:minutes:seconds
+var time;
+$("#4a").text(time);
+
+// TODO: 5. What is the current Unix timestamp?
+//var unix = moment().unix();
+var unix;
+$("#5a").text(unix);
+
+// TODO: 6. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
+var unixFormat;
+//var unixFormat = moment.unix(1318781876).format('MMM Do, YYYY');
+$("#6a").text(unixFormat);
+
